b7f29220cd862e5775242a36c79f209939ed7070
adel.belkercha <adel.belkercha@epita.fr>
2025-12-27 17:12:02 +0100

    feat: mouse rotation support

--- main.py
+++ main.py
@@ -11,15 +11,23 @@ raycaster = Raycaster(player, map)
 map_view = True
 
 clock = pygame.Clock()
+pygame.mouse.set_visible(False)
+pygame.event.set_grab(True)
 while True:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        elif event.type == pygame.MOUSEMOTION:
+            player.using_keyboard_rotation = False
     
     # general key commands
     keys = pygame.key.get_just_pressed()
+    long_press_keys = pygame.key.get_pressed()
+
+    if long_press_keys[pygame.K_RIGHT] or long_press_keys[pygame.K_d] or long_press_keys[pygame.K_LEFT] or long_press_keys[pygame.K_a]:
+        player.using_keyboard_rotation = True
 
     if keys[pygame.K_ESCAPE]: # quit
         pygame.quit()
@@ -28,7 +36,7 @@ while True:
         map_view = not map_view
 
     # update
-    player.update()
+    player.update(map_view)
     raycaster.castAllRays()
 
     # render
--- player.py
+++ player.py
@@ -1,6 +1,12 @@
 from settings import *
 import pygame
 
+def normalizeAngle(angle: float):
+    angle = angle % (2 * math.pi)
+    if (angle <= 0):
+        angle += 2 * math.pi
+    return angle
+
 class Player:
     def __init__(self) -> None:
         self.x = WIDTH // 2
@@ -9,28 +15,39 @@ class Player:
         self.turnDirection = 0
         self.walkDirection = 0
         self.rotationAngle = 0
+        self.pitchAngle = 0 # TODO later adding support to y-axis
         self.moveSpeed = 2.5
         self.rotationSpeed = math.radians(2)
+        self.using_keyboard_rotation = False
 
-    def update(self):
+    def update(self, map_view: bool):
         keys = pygame.key.get_pressed()
+        mx,my = pygame.mouse.get_pos()
         
         self.turnDirection = 0
         self.walkDirection = 0
+        self.rotationSpeed = math.radians(2)
 
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.turnDirection = 1
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.turnDirection = -1
+        # rotation
+        if self.using_keyboard_rotation:
+            if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                self.turnDirection = 1
+            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                self.turnDirection = -1
+            self.rotationAngle += self.turnDirection * self.rotationSpeed
+        else:
+            if map_view:
+                self.rotationAngle = math.atan2(my - self.y, mx - self.x)
+            else:
+                dx, _ = pygame.mouse.get_rel()
+                self.rotationAngle += dx * self.rotationSpeed * MOUSE_SENSITIVITY
+
+        # movement
         if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.walkDirection = 1
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             self.walkDirection = -1
 
-        # rotation
-        self.rotationAngle += self.turnDirection * self.rotationSpeed
-
-        # movement
         moveStep = self.moveSpeed * self.walkDirection
         self.x += math.cos(self.rotationAngle) * moveStep
         self.y += math.sin(self.rotationAngle) * moveStep
